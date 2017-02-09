2af8a3ecd2040e803a96466b78ec423af1efd1e1
猪猪 <zhuzhu@zhuzhudeMacBook-Pro.local>
2017-02-09 09:40:26 +0800

    PHLIb

---
+++ MGPhotoPicker/MGPhotoPicker.h
@@ -0,0 +1,13 @@
+//
+//  MGPhotoPicker.h
+//  MGPhotoPicker
+//
+//  Created by 猪猪 on 2017/2/7.
+//  Copyright (c) 2017年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MGPhotoPicker : NSObject
+
+@end
---
+++ MGPhotoPicker/MGPhotoPicker.m
@@ -0,0 +1,22 @@
+//
+//  MGPhotoPicker.m
+//  MGPhotoPicker
+//
+//  Created by 猪猪 on 2017/2/7.
+//  Copyright (c) 2017年 __MyCompanyName__. All rights reserved.
+//
+
+#import "MGPhotoPicker.h"
+
+@implementation MGPhotoPicker
+
+-(id)init
+{
+	if ((self = [super init]))
+	{
+	}
+    
+	return self;
+}
+
+@end
